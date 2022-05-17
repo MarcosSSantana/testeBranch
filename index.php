7e2f8aa7435f010b96511348cae4da9236fbc494
Marcos <marcosshotokan10@gmail.com>
2022-05-17 13:48:49 -0300

    abobrinha

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
     echo 'linha 2';
-    echo 'desenvolvendo nova feture';
-    echo 'dentro da dev';
+    // echo 'desenvolvendo nova feture';
+    // echo 'dentro da dev';
+    // oiasdfasd
+    // asjdhiasjdsajdj
+    // asdj
+    // <sajdasjdpas-jdpasj>dasjdasjd
+    // <as-dj>as
+    // dj-asjd-</as-dj></sajdasjdpas-jdpasj>
 ?>
